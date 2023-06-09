71ad8b43a95b23735944751307616183847aa863
nizyashka <125366967+nizyashka@users.noreply.github.com>
2023-06-09 19:00:59 +0300

    Add files via upload

--- HotelPatterns/HotelPatterns/Program.cs
+++ HotelPatterns/HotelPatterns/Program.cs
@@ -54,6 +54,7 @@ namespace HotelPatterns
     #endregion
 
     #region Адаптер
+
     // Интерфейс системы управления отелями
     interface IHotelManagementSystem
     {
@@ -61,8 +62,15 @@ namespace HotelPatterns
         void CancelBooking(string roomNumber);
     }
 
-    // Система управления отелями
-    class HotelManagementSystem : IHotelManagementSystem
+    // Интерфейс внешней системы управления отелями
+    interface IExternalHotelManagementSystem
+    {
+        void BookRoom(string roomNumber, DateTime checkInDate, DateTime checkOutDate);
+        void CancelBooking(string roomNumber);
+    }
+
+    // Внешняя система управления отелями
+    class ExternalHotelManagementSystem : IExternalHotelManagementSystem
     {
         public void BookRoom(string roomNumber, DateTime checkInDate, DateTime checkOutDate)
         {
@@ -78,11 +86,11 @@ namespace HotelPatterns
     // Адаптер для интеграции с внешней системой управления отелями
     class HotelManagementSystemAdapter : IHotelManagementSystem
     {
-        private HotelManagementSystem externalSystem;
+        private readonly IExternalHotelManagementSystem externalSystem;
 
-        public HotelManagementSystemAdapter()
+        public HotelManagementSystemAdapter(IExternalHotelManagementSystem externalSystem)
         {
-            externalSystem = new HotelManagementSystem();
+            this.externalSystem = externalSystem;
         }
 
         public void BookRoom(string roomNumber, DateTime checkInDate, DateTime checkOutDate)
@@ -206,6 +214,160 @@ namespace HotelPatterns
             // Другие действия после подтверждения бронирования
         }
     }
+
+    class Room_State
+    {
+        private RoomState currentState;
+
+        public Room_State()
+        {
+            TransitionTo(new NotRentedState());
+        }
+
+        public void TransitionTo(RoomState state)
+        {
+            currentState = state;
+            currentState.SetRoom(this);
+        }
+
+        public void RentRoom()
+        {
+            currentState.RentRoom();
+        }
+
+        public void CheckOutRoom()
+        {
+            currentState.CheckOutRoom();
+        }
+
+        public void CleanRoom()
+        {
+            currentState.CleanRoom();
+        }
+
+        public void RepairRoom()
+        {
+            currentState.RepairRoom();
+        }
+    }
+
+    // Абстрактное состояние комнаты
+    abstract class RoomState
+    {
+        protected Room_State room;
+
+        public void SetRoom(Room_State room)
+        {
+            this.room = room;
+        }
+
+        public abstract void RentRoom();
+        public abstract void CheckOutRoom();
+        public abstract void CleanRoom();
+        public abstract void RepairRoom();
+    }
+
+    // Конкретное состояние: комната сдана
+    class RentedState : RoomState
+    {
+        public override void RentRoom()
+        {
+            Console.WriteLine("Room is already rented.");
+        }
+
+        public override void CheckOutRoom()
+        {
+            Console.WriteLine("Checking out the room.");
+            // Переход к состоянию "не сдана"
+            room.TransitionTo(new NotRentedState());
+        }
+
+        public override void CleanRoom()
+        {
+            Console.WriteLine("Cannot clean a rented room.");
+        }
+
+        public override void RepairRoom()
+        {
+            Console.WriteLine("Cannot repair a rented room.");
+        }
+    }
+
+    // Конкретное состояние: комната не сдана
+    class NotRentedState : RoomState
+    {
+        public override void RentRoom()
+        {
+            Console.WriteLine("Renting the room.");
+            // Переход к состоянию "сдана"
+            room.TransitionTo(new RentedState());
+        }
+
+        public override void CheckOutRoom()
+        {
+            Console.WriteLine("Cannot check out a room that is not rented.");
+        }
+
+        public override void CleanRoom()
+        {
+            Console.WriteLine("Cleaning the room.");
+            // Переход к состоянию "уборка"
+            room.TransitionTo(new CleaningState());
+        }
+
+        public override void RepairRoom()
+        {
+            Console.WriteLine("Cannot repair a room that is not rented.");
+        }
+    }
+
+    // Конкретное состояние: уборка комнаты
+    class CleaningState : RoomState
+    {
+        public override void RentRoom()
+        {
+            Console.WriteLine("Cannot rent a room that is being cleaned.");
+        }
+
+        public override void CheckOutRoom()
+        {
+            Console.WriteLine("Cannot check out a room that is being cleaned.");
+        }
+
+        public override void CleanRoom()
+        {
+            Console.WriteLine("Room is already being cleaned.");
+        }
+
+        public override void RepairRoom()
+        {
+            Console.WriteLine("Cannot repair a room that is being cleaned.");
+        }
+    }
+
+    // Конкретное состояние: комната в ремонте
+    class RepairState : RoomState
+    {
+        public override void RentRoom()
+        {
+            Console.WriteLine("Cannot rent a room that is under repair.");
+        }
+
+        public override void CheckOutRoom()
+        {
+            Console.WriteLine("Cannot check out a room that is under repair.");
+        }
+
+        public override void CleanRoom()
+        {
+            Console.WriteLine("Cannot clean a room that is under repair.");
+        }
+
+        public override void RepairRoom()
+        {
+            Console.WriteLine("Room is already under repair.");
+        }
+    }
     #endregion
 
     class HotelPatterns
@@ -220,7 +382,8 @@ namespace HotelPatterns
             Room budgetRoom = budgetHotel.CreateRoom();
             budgetRoom.Display();
 
-            IHotelManagementSystem hotelManagementSystem = new HotelManagementSystemAdapter();
+            IExternalHotelManagementSystem externalHotelManagementSystem = new ExternalHotelManagementSystem();
+            IHotelManagementSystem hotelManagementSystem = new HotelManagementSystemAdapter(externalHotelManagementSystem);
             hotelManagementSystem.BookRoom("101", DateTime.Now, DateTime.Now.AddDays(3));
             hotelManagementSystem.CancelBooking("101");
 
@@ -229,6 +392,13 @@ namespace HotelPatterns
             bookingContext.SelectDate(DateTime.Now);
             bookingContext.SelectRoom("101");
             bookingContext.ConfirmBooking();
+
+            Room_State room = new Room_State();
+            room.RentRoom(); // Комната сдана
+            room.CheckOutRoom(); // Комната не сдана
+            room.CleanRoom(); // Комната в уборке
+            room.RepairRoom(); // Комната в ремонте
+            room.CleanRoom(); //Убрать комнату, которая уже убирается
         }
     }
 }
